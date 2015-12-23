f7e0baea3fb1fd34e9c7bd2a3803c6d6fb9f838d
DengYiping <scottdeng@live.cn>
2015-12-22 22:20:16 -0600

    Initial Commit

---
+++ RESTpp/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  RESTpp
+//
+//  Created by Scott Deng on 12/22/15.
+//  Copyright © 2015 Scott Deng. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+    return 0;
+}
